60b1c0a0c05596a2f18b25c5d9059b87b6f8fed4
Darren Conley <dcprime@gmail.com>
2014-02-04 09:10:35 -0500

    Added several methods to blackjack1

--- blackjack1.rb
+++ blackjack1.rb
@@ -1,49 +1,122 @@
-# Two arrays used for selection of cards being dealt
-suit = [' of hearts', ' of spades', ' of diamonds', ' of clubs']
-rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
-# Array to store which cards have already been dealt
-dealt = []
-# Two variables to store the values (totals) of each hand - player and dealer
+player_cards = []
+dealer_cards = []
 player_value = 0
 dealer_value = 0
 
-# In this method the recipient would be either the player or dealer, 
-# so when the method is called, it's called as 'deal player' or 'deal dealer'
-def deal recipient
-	# Get a random card rank from the 'rank' array above
-	card_rank = rank[rand(13)] 
-	# Get a random card suit from the 'suit' array above
-	card_suit = [rand(4)]
-		# Statements to set returned card value (or add it to existing total value of cards)
-		# for either the 'player' or 'dealer' (depending on which was passed to the method)
-		if card_rank == ('Jack' || 'Queen' || 'King')
-			# card value is '10' for ranks that are strings (apart from 'Ace')
-			recipient + '_value' = 10
-			# Calculate card value for Ace (needs to be re-done to accommodate cards dealt after
-			# 'Ace' that might cause bust if Ace valued at 11, but for now let's try this)
-			elsif card_rank == 'Ace'
-				if (recipient + 'value') + 11 > 21
-					recipient + 'value' = 1
-				else
-					recipient + 'value' = 11
-				end
-			else 
-				# Card value is equal to the integer value of chosen 'rank' array slot
-				recipient + '_value' = (recipient + '_value') + card_rank 
-		end
-	# Generate the name of the card dealt (eg. 'Jack of hearts' or '6 of spades')
+# Generate a random card (returns card)
+def random_card
+suit = [' of hearts', ' of spades', ' of diamonds', ' of clubs']
+rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
+	card_rank = rank[rand(13)]
+	card_suit = suit[rand(4)]
 	card = card_rank.to_s + card_suit
-		# Check the 'dealt' array to see if this card has already been dealt. If so,
-		# run 'deal recipient' again (need to reset calculated value?)
-		while dealt.each { |dealt_card| dealt_card == card }
-			deal recipient
-		end
-	# Add the dealt card to the 'dealt' array
-	dealt << card
-	# Return the name of the card
-	card
+  card
 end
 
-card_rank = rank[rand(13)] 
-card_suit = suit[rand(4)]
-card = card_rank.to_s + card_suit
+# Check if card has been already dealt
+def deal recipient_array, other_array 
+  dealt_card = random_card
+	dealt_array = recipient_array + other_array
+	if dealt_array.include?(dealt_card) == true
+	  deal recipient_array, other_array
+  else
+	  return dealt_card
+  end
+end
+
+# Use these 2 methods for generating player card or dealer card
+def deal_player player_array, dealer_array
+  card = deal player_array, dealer_array
+  player_array << card
+  return card	
+end
+
+def deal_dealer dealer_array, player_array
+  card = deal dealer_array, player_array
+  dealer_array << card
+  return card	
+end
+
+# Methods for generating card values
+def array_num_total array
+  i = 0
+	num_total = 0
+	integer_array = array.collect { |item| item.to_i }
+	while i < integer_array.length
+	  num_total = integer_array[i] + num_total
+		i = i + 1
+	end
+  num_total
+end
+
+def face_card_total array
+  face_total = 0
+  face_array = array.collect { |item| item[0] }
+	face_array.delete_if { |item| item.to_i > 0 || item == 'A'}
+	face_total = face_array.length * 10
+	face_total
+end
+
+# Use this method for generating card array total values
+def card_total array
+  i = 0
+  total = 0
+	ace_value = 0
+	ace_array = []
+	array_test = []
+	total = (array_num_total array) + (face_card_total array)
+	ace_array = array.select { |item| item[0] == 'A' }
+	array_test = array.each { |item| item[0] }
+	if (ace_array.length == 1) && (total == 10) 
+	  total = 21
+		return total
+	end
+	if ace_array.eql?(array_test)
+	  total = 10 + ace_array.length
+		return total
+	end
+	  while i < ace_array.length
+		  if total + 11 >= 21
+			  ace_value = 1
+			else
+			  ace_value = 11
+			end
+      total = total + ace_value 
+			i = i + 1
+		end
+  total
+end
+
+
+
+# Generate 1 dealer card **(random_card + dealt_check) + add to dealer_cards array**
+# Generate dealer total **iterate over dealer_cards array (with Ace check)**
+# Generate 2 player cards **(random_card + dealt_check) + add to player_cards array**
+# Generate player total **iterate over player_cards array (with Ace check)**
+#   If player blackjack, do dealer card sequence
+# Player input: [H]it or [S]tay
+#   If 'Hit', generate 1 player card
+#   Generate player total (with 'Ace check')
+#     If 'bust' dealer wins
+#     Else player input (loop till 'stay' or 'bust')
+#   If 'Stay', do dealer card sequence
+#   Else 'Please enter [H]it or [S]tay
+# Dealer card sequence:
+# Generate 1 dealer card
+# Generate dealer total (with 'Ace check')
+#   If dealer blackjack, compare totals
+#   If dealer total < 17 generate 1 dealer card (loop)
+#   If dealer total >= 17 stay
+# Compare totals
+#   If player total > dealer total, player wins
+#   If dealer total > player total, dealer wins
+#   If player total = dealer total, tie 
+
+# Generate random card
+# Check if card was dealt
+# If already dealt, generate a new card
+# If not already dealt 
+#  (1) add card to dealt array, 
+#  (2) calculate card value
+#  (3) add card value to player or dealer
+
