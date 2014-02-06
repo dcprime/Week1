1c13a185b22b78e91adb8c2ec7e84c6e47545630
Darren Conley <dcprime@gmail.com>
2014-02-06 15:11:17 -0500

    Added roman numeral convertor

---
+++ Chapter12_roman_1999.rb
@@ -0,0 +1,50 @@
+# Program to change Roman numerals to regular numbers
+
+def roman_specials roman_numeral
+  del_index = 0
+  specials_total = 0
+  specials = {}
+  specials['IV'] = 4
+  specials['IX'] = 9
+  specials['XL'] = 40
+  specials['XC'] = 90
+  specials['CD'] = 400
+  specials['CM'] = 900
+    specials.each do |k, v| 
+      if roman_numeral.upcase.include?(k)
+	    specials_total = specials_total + v
+		del_index = roman_numeral.upcase.index(k)
+		2.times {roman_numeral.delete!(roman_numeral[del_index])}
+	  end
+    end
+  specials_total
+end
+
+def roman_regulars roman_numeral
+  regulars_total = 0
+  regulars = {}
+  regulars['M'] = 1000
+  regulars['D'] = 500
+  regulars['C'] = 100
+  regulars['L'] = 50
+  regulars['X'] = 10
+  regulars['V'] = 5
+  regulars['I'] = 1
+    regulars.each do |k, v| 
+      if roman_numeral.upcase.include?(k)
+	    regulars_total = regulars_total + ((roman_numeral.count k) * v)
+		roman_numeral.delete!(roman_numeral[k])
+	  end
+    end
+  regulars_total
+end
+
+puts 'Welcome to the Roman Numeral Interpreter!'
+puts 'Enter any Roman numeral and press [Enter]'
+puts 'The regular number will then be returned'
+print 'Enter Roman numeral here =>'
+roman = gets.chomp
+
+total = 0
+total = (roman_specials roman) + (roman_regulars roman)
+puts "Your converted number is #{total}"
